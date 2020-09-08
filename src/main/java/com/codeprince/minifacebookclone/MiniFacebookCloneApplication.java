f233892122e50ebdd1577a9201a1e56ce8f8ec3b
Prince <mac@Prince.local>
2020-09-08 01:56:40 +0100

    initial commit

---
+++ src/main/java/com/codeprince/minifacebookclone/MiniFacebookCloneApplication.java
@@ -0,0 +1,13 @@
+package com.codeprince.minifacebookclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFacebookCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniFacebookCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeprince/minifacebookclone/MiniFacebookCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeprince.minifacebookclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFacebookCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
